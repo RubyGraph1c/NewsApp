49abd0d43e745b98935aa34c304fc6cc44f23ff9
Lucinda Shale <lucindashale@yahoo.com>
2021-12-03 13:58:06 +0000

    file setup for new project - front and backend

---
+++ NewsAppService/src/main/java/com/example/codeclan/NewsAppService/NewsAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.NewsAppService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAppServiceApplication.class, args);
+	}
+
+}
